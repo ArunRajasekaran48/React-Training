e5ffed49d1af89b13f869ca5451527936dc08912
ArunRajasekaran48 <arun.r2023cse@sece.ac.in>
2025-07-15 21:40:12 +0530

    Day 11 AN commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
-import Login from "./components/Login"
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Home from './components/Home'
+import Login from './components/Login'
+import Register from './components/Register'
+import { BrowserRouter , Routes , Route } from 'react-router-dom'
+import Dashboard from './components/Dashboard'
 
 function App() {
-  return(
-    <div>
-      <Login />
-    </div>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path = "/" element = {<Home/>}/>
+        <Route path = "/register" element = {<Register/>}/>
+        <Route path = "/login" element = {<Login/>}/>
+        <Route path = "/dashboard" element = {<Dashboard/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
